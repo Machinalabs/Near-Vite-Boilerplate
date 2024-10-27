09f293ed9e6c8ece9cbb439e5a16c8fe6adf1775
edsonalcala <edson_alcala@msn.com>
2024-10-27 09:18:40 +0000

    fix: added tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,9 @@ export default function HomePage() {
 
     return (
         <>
-            <h1>Home Page!!!</h1>
+            <h1 className="text-5xl font-bold underline">
+                Hello world!
+            </h1>
             <p>{signedAccountId}</p>
         </>
     )
